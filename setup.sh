5ca0be0d6dc199fe61f7cdf7f0df05e54c2df22c
AlnurElberier <a.elberier@gmail.com>
2024-10-24 16:16:35 -0700

    make path absolute

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 
 export ARG_GG_VERSION=2.13.0
 
+
 USER_NME=$1
 
-echo $USER_NME
+export GG_INSTALLER_PATH="/home/$USER_NME/GreengrassInstaller"
+echo $GG_INSTALLER_PATH
 
 gen_id()
 {
@@ -45,24 +47,24 @@ sudo groupadd --system ggc_group
  
 # Download and unzip greengrass installer
 curl -s https://d2s8p88vqu9w66.cloudfront.net/releases/greengrass-$ARG_GG_VERSION.zip > greengrass-nucleus-latest.zip
-unzip greengrass-nucleus-latest.zip -d GreengrassInstaller && rm greengrass-nucleus-latest.zip
+unzip greengrass-nucleus-latest.zip -d $GG_INSTALLER_PATH && rm greengrass-nucleus-latest.zip
  
 # Download fleet provisioning plugin
-curl -s https://d2s8p88vqu9w66.cloudfront.net/releases/aws-greengrass-FleetProvisioningByClaim/fleetprovisioningbyclaim-latest.jar > GreengrassInstaller/aws.greengrass.FleetProvisioningByClaim.jar
+curl -s https://d2s8p88vqu9w66.cloudfront.net/releases/aws-greengrass-FleetProvisioningByClaim/fleetprovisioningbyclaim-latest.jar > $GG_INSTALLER_PATH/aws.greengrass.FleetProvisioningByClaim.jar
 
 # Echo Greengrass installer version
-java -jar ./GreengrassInstaller/lib/Greengrass.jar --version
+java -jar $GG_INSTALLER_PATH/lib/Greengrass.jar --version
 
 # Copy config file
-sudo cp ./config.json ./GreengrassInstaller/config.json
+sudo cp ./config.json $GG_INSTALLER_PATH/config.json
  
 # Run installer
 sudo -E java -Droot="/greengrass/v2" -Dlog.store=FILE \
-  -jar ./GreengrassInstaller/lib/Greengrass.jar \
-  --trusted-plugin ./GreengrassInstaller/aws.greengrass.FleetProvisioningByClaim.jar \
-  --init-config ./GreengrassInstaller/config.json \
+  -jar $GG_INSTALLER_PATH/lib/Greengrass.jar \
+  --trusted-plugin $GG_INSTALLER_PATH/aws.greengrass.FleetProvisioningByClaim.jar \
+  --init-config $GG_INSTALLER_PATH/config.json \
   --component-default-user ggc_user:ggc_group \
   --setup-system-service true
  
 # Delete GreengrassInstaller
- sudo rm -rf /home/$USER_NME/GreengrassInstaller/
+ sudo rm -rf $GG_INSTALLER_PATH/
