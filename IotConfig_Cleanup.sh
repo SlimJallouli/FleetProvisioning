438d890402fb73b7e75215004d451065a6267e10
SlimJallouli <slim.jallouli@st.com>
2024-11-06 08:37:49 -0800

    Add license

--- IotConfig_Cleanup.sh
+++ IotConfig_Cleanup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#******************************************************************************
+# * @file           : IotConfig_Cleanup.sh
+# * @brief          : 
+# ******************************************************************************
+# * @attention
+# *
+# * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+# * All rights reserved.</center></h2>
+# *
+# * This software component is licensed by ST under BSD 3-Clause license,
+# * the "License"; You may not use this file except in compliance with the
+# * License. You may obtain a copy of the License at:
+# *                        opensource.org/licenses/BSD-3-Clause
+# ******************************************************************************
+
 # Function to display help
 usage() {
     echo "Usage: $0 -t THING_NAME"
--- execute.sh
+++ execute.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#******************************************************************************
+# * @file           : execute.sh
+# * @brief          : 
+# ******************************************************************************
+# * @attention
+# *
+# * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+# * All rights reserved.</center></h2>
+# *
+# * This software component is licensed by ST under BSD 3-Clause license,
+# * the "License"; You may not use this file except in compliance with the
+# * License. You may obtain a copy of the License at:
+# *                        opensource.org/licenses/BSD-3-Clause
+# ******************************************************************************
+
 # Define the YAML file
 TEMPLATE_FILE="template.yaml"
 
